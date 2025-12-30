673ae718205fb5d0b7250cbc13b43bb25b48340c
JAYAPRAKASH\asus <ajaypreksh.6@gmail.com>
2025-12-31 01:06:36 +0530

    Implement logger functionality with file output and thread safety; update logger API

--- src/server/server_logger.h
+++ src/server/server_logger.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <windows.h>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <string>
+#include <fstream>
+#include "../../common/config/config.h"
 
 /* Log levels */
 enum class LogLevel {
@@ -9,7 +17,21 @@ enum class LogLevel {
 };
 
 /* Logger API */
-void logger_init();
+void logger_init(const Config& cfg);
+void logger_shutdown();
 void log_debug(const char* fmt, ...);
 void log_info(const char* fmt, ...);
 void log_error(const char* fmt, ...);
+
+/* Internal structures */
+struct LogEntry {
+    LogLevel level;
+    std::string message;
+};
+
+extern std::queue<LogEntry> logQueue;
+extern std::mutex logMutex;
+extern std::condition_variable logCV;
+extern std::atomic<bool> loggerRunning;
+extern std::thread logWorker;
+extern std::ofstream logFile;
